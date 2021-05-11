28e74106debd5945bfc7df76e630c71e334b0da4
s21berkeucvet <berkeucvet@sabanciuniv.edu>
2021-05-11 18:43:39 +0300

    bot sync try

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+function handleSubmit() {
+    var message = $('#url').val()
+    var telegram_bot_id = $('#botid').val()
+    var chat_id = $('#chatid').val()
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.telegram.org/" + telegram_bot_id + "/sendMessage",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache"
+    },
+    "data": JSON.stringify({
+      "chat_id": chat_id,
+      "text": message
+    })
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  }); 
+}
